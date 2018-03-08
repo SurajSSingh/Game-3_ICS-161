26df42ae6d4768051642e503d34327872d271a42
Suraj Singh <surajss@uci.edu>
2018-03-08 10:58:58 -0800

    Added Win/Lose State
    Win and Lose state is now active. Also added some obstacles.

--- Assets/Scripts/NetworkedWinState.cs
+++ Assets/Scripts/NetworkedWinState.cs
@@ -6,26 +6,28 @@ using UnityEngine.UI;
 
 public class NetworkedWinState : NetworkBehaviour {
 
+	private Text infoText;
+
 	void OnTriggerEnter2D (Collider2D coll){
 		if (isServer) {
 			if (coll.CompareTag ("State")) {
 				RpcWin ();
+
 			}
 		}
 	}
 
 	[ClientRpc]
 	void RpcWin (){
-		Text infoText = null;
-		Text[] allTexts = GameObject.FindObjectsOfType<Text> ();
-		foreach (Text t in allTexts) {
-			if (t.tag == "State") {
+		Text[] infoArray = GameObject.FindObjectsOfType<Text> ();
+		foreach (Text t in infoArray){
+			if (t.CompareTag ("Screen")) {
 				infoText = t;
-				break;
 			}
 		}
+		//Debug.Log (infoText);
 		if (infoText != null) {
-			infoText.enabled = true;
+			infoText.color = Color.cyan;
 			if (isLocalPlayer) {
 				infoText.text = "You Win";
 			} else {
