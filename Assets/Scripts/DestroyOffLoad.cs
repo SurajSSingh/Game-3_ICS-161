7f782cabaf70ab0072e7eac98b56e8e8954d4edd
Suraj Singh <surajss@uci.edu>
2018-03-11 11:52:58 -0700

    Network Lobby
    Created Multiplayer Lobby. To add car and track selection.

---
+++ Assets/Scripts/DestroyOffLoad.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOffLoad : MonoBehaviour {
+	//Destroys game objects when called
+
+	public void DestroyUndestroyed (GameObject go)
+	{
+		Destroy (go);
+	}
+}
---
+++ Assets/Scripts/MenuAction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuAction : MonoBehaviour {
+
+	public void ShowMenu (GameObject Menu)
+	{
+		Menu.SetActive (true);
+	}
+
+	public void CloseMenu (GameObject Menu)
+	{
+		if (Menu.activeInHierarchy == true) {
+			Menu.SetActive (false);
+		}
+	}
+}
