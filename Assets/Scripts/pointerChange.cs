ce696878aa4ddb50aad2576d7bc60df31041020d
Suraj Singh <surajss@uci.edu>
2018-03-14 22:24:25 -0700

    Very Simple AI
    AI is present but it is not very smart. Very unlikely to beat the
    player, but it follows each waypoint well.

--- Assets/Scripts/pointerChange.cs
+++ Assets/Scripts/pointerChange.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 public class pointerChange : MonoBehaviour {
 
 	[SerializeField]
-	private int trackNumber = 0;
-
-	[SerializeField]
 	private List<Transform> nodes = new List<Transform>();
 
 	private int current = 0;
@@ -18,22 +15,22 @@ public class pointerChange : MonoBehaviour {
 		current = 0;
 		active = false;
 
-//		switch{
-//
-//		GameObject track = ;
-//		}
+		GameObject pathway = GameObject.FindGameObjectWithTag("Pathway");
 
-		Transform[] pathNodes = GetComponentsInChildren<Transform>();
-		nodes = new List<Transform>();
 
-		for (int i = 0; i < pathNodes.Length; ++i){
-			if(pathNodes[i] != this.transform){
-				nodes.Add (pathNodes[i]);
+		if(pathway != null){
+			Transform[] pathNodes = pathway.GetComponentsInChildren<Transform> ();
+			nodes = new List<Transform>();
+
+			for (int i = 0; i < pathNodes.Length; ++i){
+				if(pathNodes[i] != this.transform){
+					nodes.Add (pathNodes[i]);
+				}
 			}
-		}
-		if(nodes.Count > 0){
-			this.transform.position = nodes [current].position;
-			active = true;
+			if(nodes.Count > 0){
+				this.transform.position = nodes [current].position;
+				active = true;
+			}
 		}
 	}
 
